0023b44f38904543d6b4c1e13ed7da45cf3c92ca
ashiq24 <ashiqbuet14@gmail.com>
2026-05-02 18:15:29 -0400

    sweep_anvil.sh

--- temp/conversion.sh
+++ temp/conversion.sh
@@ -1,5 +1,5 @@
 bash hugging_face_releases/auto_release.sh \
-      --command "python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours --soft_thresholding 1.0 --soft_thresholding_pos 0.90 --lr 0.001 --backbone_lr 0.00005 --n_rotations 720 --eval_rot 30 --warmup_steps 250 --seed 420000 --hard_mask --weight_decay 0.07" \
-      --checkpoint ./logs/segmentation/wandb/run-20260501_163709-qwi3n34z/files/best.pt
+      --command "python seg_main.py --config config/segmentation.yaml --config_name dinov2_seg_c18 --run_name dinov2_seg_c18_ours_180_9 --soft_thresholding 0.9 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 250 --seed 42000 --hard_mask --weight_decay 0.01" \
+      --checkpoint ./logs/segmentation/wandb/run-20260502_035654-l9ub5rbv/files/best.pt
 
 huggingface-cli upload ashiq24/softeq-vit-base-patch16-224-voc-seg-c720-s0.90 hugging_face_releases/filtered-vit-base-patch16-224-voc-seg-c720-s0.90
