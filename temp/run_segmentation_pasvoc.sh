b0a792dc1e2983a33d27e9611f0c6409e309cc66
ashiq24 <ashiqbuet14@gmail.com>
2026-05-05 14:05:34 -0400

    sweep_anvil.sh

--- temp/run_segmentation_pasvoc.sh
+++ temp/run_segmentation_pasvoc.sh
@@ -1,12 +1,38 @@
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_1_7 --soft_thresholding 1.0 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 42 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_1_7 --soft_thresholding 1.0 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 420 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_1_7 --soft_thresholding 1.0 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 4200 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_1_7 --soft_thresholding 1.0 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 42000 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_1_7 --soft_thresholding 1.0 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 420000 --hard_mask --weight_decay 0.07
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_180_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_180_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 420 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_180_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 4200 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_180_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42000 --weight_decay 0.1
 
 
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_9_9 --soft_thresholding 0.9 --soft_thresholding_pos 0.9 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 42 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_9_9 --soft_thresholding 0.9 --soft_thresholding_pos 0.9 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 420 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_9_9 --soft_thresholding 0.9 --soft_thresholding_pos 0.9 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 4200 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_9_9 --soft_thresholding 0.9 --soft_thresholding_pos 0.9 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 42000 --hard_mask --weight_decay 0.07
-python seg_main.py --config config/segmentation.yaml --config_name vit_seg_c18 --run_name vit_seg_c18_ours_4_9_9 --soft_thresholding 0.9 --soft_thresholding_pos 0.9 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 420000 --hard_mask --weight_decay 0.07
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_180_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_180_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 420
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_180_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 4200
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_180_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42000
+
+
+
+### c 4
+
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_4_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_4_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 420 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_4_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 4200 --weight_decay 0.1
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name vit_seg_c18_4_8_8 --run_name vit_seg_c18_ade_ours_180_8_8_n --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00005 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42000 --weight_decay 0.1
+
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_4_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_4_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 420
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_4_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 4200
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18_4_8_1 --run_name dinov2_seg_c18_ade_ours_8_1_n --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42000
