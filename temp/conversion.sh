089edd651bc63188a68a431b655f69117f7a56d9
ashiq24 <ashiqbuet14@gmail.com>
2026-05-09 22:21:17 -0400

    temp

--- temp/conversion.sh
+++ temp/conversion.sh
@@ -1,5 +1,7 @@
 bash hugging_face_releases/auto_release.sh \
-      --command "python seg_main.py --config config/segmentation.yaml --config_name dinov2_seg_c18 --run_name dinov2_seg_c18_ours_4_9_1 --soft_thresholding 0.9 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 250 --seed 420000 --hard_mask --weight_decay 0.01" \
-      --checkpoint ./logs/segmentation/wandb/run-20260504_001020-ff4v517g/files/best.pt
+      --command "python seg_main.py --config config/segmentation_ade.yaml --config_name dinov2_seg_c18 --run_name dinov2_seg_c18_ade_ours_8_1 --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.00001 --n_rotations 4 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42000" \
+      --checkpoint ./logs/ade20k/wandb/run-20260505_190418-jm4dlevc/files/best.pt
 
-huggingface-cli upload ashiq24/softeq-dinov2-base-voc-seg-c4-s0.9-sp1.0 hugging_face_releases/filtered-dinov2-base-voc-seg-c4-s1.0
+
+
+huggingface-cli upload ashiq24/softeq-dinov2-base-ade20k-seg-c4-s0.8-sp1.0  ./hugging_face_releases/filtered-dinov2-base-ade-seg-c4-s1.0
