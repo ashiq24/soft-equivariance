5b3df7444a03510532af16657d00ff73bf19c29c
ashiq24 <ashiqbuet14@gmail.com>
2026-05-10 01:21:32 -0400

    sweep_anvil.sh

--- temp/run_segmentation_pasvoc.sh
+++ temp/run_segmentation_pasvoc.sh
@@ -1,3 +1,21 @@
-python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c18_ade_ours_8_8 --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.000005 --n_rotations 180 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c36_ade_ours_8_8 --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.000005 --n_rotations 360 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c36_ade_ours_7_7 --soft_thresholding 0.7 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.000005 --n_rotations 360 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c36_ade_ours_8_1 --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.000005 --n_rotations 360 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c36_ade_ours_7_1 --soft_thresholding 0.7 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.000005 --n_rotations 360 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
 
 
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_8_8 --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_7_7 --soft_thresholding 0.7 --soft_thresholding_pos 0.7 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_8_1 --soft_thresholding 0.8 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_7_1 --soft_thresholding 0.7 --soft_thresholding_pos 1.0 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_4_4 --soft_thresholding 0.4 --soft_thresholding_pos 0.4 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
+
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c4_ade_ours_1_1 --soft_thresholding 0.1 --soft_thresholding_pos 0.1 --lr 0.001 --backbone_lr 0.000005 --n_rotations 4 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
