e6ad449ba3bc693d879754d16c69ebb512df4765
ashiq24 <ashiqbuet14@gmail.com>
2026-05-09 23:47:44 -0400

    sweep_anvil.sh

--- temp/run_segmentation_pasvoc.sh
+++ temp/run_segmentation_pasvoc.sh
@@ -1,3 +1,3 @@
-python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c18_ade_ours_8_8 --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.00001 --n_rotations 180 --eval_rot 30 --warmup_steps 500 --hard_mask --seed 42
+python seg_main.py --config config/segmentation_ade.yaml --config_name dinov3_seg --run_name dinov3_seg_c18_ade_ours_8_8 --soft_thresholding 0.8 --soft_thresholding_pos 0.8 --lr 0.001 --backbone_lr 0.000005 --n_rotations 180 --eval_rot 30 --warmup_steps 1000 --hard_mask --seed 42
 
 
